9df38d79d4bfdf8ea4c1b9c931a5bfaae0b7b9fa
samuelea <batonenbois@gmail.com>
2024-07-08 11:27:19 -0400

    kit react boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# kit-react-boilerplate
+# Sequence Kit Starter Boilerplate - Fork me!
+
+Starter Sequence Kit Boilerplate that can be freely copied or forked for projects.
+
+
+## Start Locally
+The app will start in localhost port 5173
+
+```js
+  pnpm install && pnpm dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import Home from './views/Home'
+import { KitProvider } from '@0xsequence/kit'
+import { getDefaultConnectors } from '@0xsequence/kit-connectors'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createConfig, http, WagmiProvider } from 'wagmi'
+import { mainnet, polygon, Chain } from 'wagmi/chains'
+
+const queryClient = new QueryClient()
+
+const App = () => {
+  const chains = [mainnet, polygon] as [Chain, ...Chain[]]
+
+  // Get your own project access key on https://sequence.build
+  const projectAccessKey = 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
+
+  const connectors = getDefaultConnectors({
+    walletConnectProjectId: 'wallet-connect-id',
+    defaultChainId: 137,
+    appName: 'demo app',
+    projectAccessKey
+  })
+
+  const transports: { [key: number]: any } = {}
+
+  chains.forEach(chain => {
+    transports[chain.id] = http()
+  })
+
+  const config = createConfig({
+    transports,
+    connectors,
+    chains
+  })
+
+  const kitConfig = {
+    projectAccessKey
+  }
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <KitProvider config={kitConfig}>
+          <Home />
+        </KitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
